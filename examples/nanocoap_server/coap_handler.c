9272809b9c877a5c578a50046296513694b85593
Koen Zandberg <koen@bergzand.net>
2018-09-28 11:25:20 +0200

    fixup! fixup! fixup! fixup! examples/nanocoap: add blockwise block2 example

--- examples/nanocoap_server/coap_handler.c
+++ examples/nanocoap_server/coap_handler.c
@@ -32,12 +32,13 @@ static ssize_t _riot_block2_handler(coap_pkt_t *pkt, uint8_t *buf, size_t len, v
 {
     (void)context;
     coap_block_slicer_t slicer;
+    coap_block2_init(pkt, &slicer);
     uint8_t *payload = buf + coap_get_total_hdr_len(pkt);
 
     uint8_t *bufpos = payload;
 
     bufpos += coap_put_option_ct(bufpos, 0, COAP_FORMAT_TEXT);
-    bufpos += coap_block2_init(bufpos, COAP_OPT_CONTENT_FORMAT, pkt, &slicer);
+    bufpos += coap_opt_put_block2(bufpos, COAP_OPT_CONTENT_FORMAT, &slicer, 1);
     *bufpos++ = 0xff;
 
     /* Add actual content */
