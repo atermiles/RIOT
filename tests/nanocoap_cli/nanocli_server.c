91ff56ebabb68de5eb93130875e362b04ea660ba
Ken Bannister <kb2ma@runbox.com>
2018-12-23 08:26:46 -0500

    fixup! allow specification of server port

--- tests/nanocoap_cli/nanocli_server.c
+++ tests/nanocoap_cli/nanocli_server.c
@@ -27,23 +27,38 @@
 #define ENABLE_DEBUG (0)
 #include "debug.h"
 
-static void _start_server(void)
+static void _start_server(uint16_t port)
 {
     uint8_t buf[128];
-    sock_udp_ep_t local = { .port=COAP_PORT, .family=AF_INET6 };
+    sock_udp_ep_t local = { .port=port, .family=AF_INET6 };
     nanocoap_server(&local, buf, sizeof(buf));
 }
 
 int nanotest_server_cmd(int argc, char **argv)
 {
-    if (argc == 2) {
-        if (strncmp("start", argv[1], 5) == 0) {
-            puts("starting server\n");
-            _start_server();
-            return 0;
+    uint16_t port = COAP_PORT;
+    if (argc >= 2) {
+        if (strncmp("start", argv[1], 5) != 0) {
+            goto end;
         }
+
+        if (argc == 3) {
+            port = atoi(argv[2]);
+            if (port == 0) {
+                puts("nanotest: unable to parse port");
+                return 1;
+            }
+        }
+
+        puts("starting server\n");
+        _start_server(port);
+
+        /* server executes run loop; never reaches this point*/
+        return 0;
     }
 
-    printf("usage: %s start\n", argv[0]);
+    end:
+    printf("usage: %s start [port]\n", argv[0]);
+    printf("       Port defaults to %u\n", COAP_PORT);
     return 1;
 }
